656833431f6fbf254bd89830445683d9651bb8ec
dolordprince <dolor52399@gmail.com>
2026-02-25 20:02:31 +0100

    Add Nix flake configuration file flake.nix

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{ # Nix flake configuration for the DOLOR_SMM project  
+  description = "A Nix flake for the DOLOR_SMM project";  
+  outputs = {  
+    packages.x86_64-linux.default = {  
+      # Define your packages here  
+    };  
+  };  
+}
